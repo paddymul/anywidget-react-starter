d75bae52c8afae77f6b703572a744c865b6e2196
Paddy Mullen <paddy@paddymullen.com>
2024-12-11 17:00:14 -0500

    changed some-react-lib to only import recharts, still getting Uncaught TypeError: st.current is null

--- packages/some-react-tsxlib/src/index.ts
+++ packages/some-react-tsxlib/src/index.ts
@@ -1,50 +1,12 @@
 // Copyright (c) Paddy Mullen
 // Distributed under the terms of the Modified BSD License.
 
-import { ColumnsEditor } from "./components/ColumnsEditor";
-import { DFViewer } from "./components/DFViewerParts/DFViewer";
-import {
-    DFViewerInfinite,
-    StaticWrapDFViewerInfinite,
-} from "./components/DFViewerParts/DFViewerInfinite";
 
-import { WidgetDCFCell } from "./components/DCFCell";
-import { BuckarooInfiniteWidget } from "./components/BuckarooWidgetInfinite";
-
-import { HistogramCell } from "./components/DFViewerParts/HistogramCell";
-import { InfiniteEx } from "./components/DFViewerParts/TableInfinite";
-
-import { DependentTabs } from "./components/DependentTabs";
-
-import { OperationViewer } from "./components/Operations";
-import * as CommandUtils from "./components/CommandUtils";
-import * as utils from "./components/utils";
-import { StatusBar } from "./components/StatusBar";
-import '@ag-grid-community/styles/ag-grid.css'; 
-import '@ag-grid-community/styles/ag-theme-quartz.css';
-import './style/dcf-npm.css'
-import * as versionInfo from "./version";
-import * as widgetUtils from "./widgetUtils";
-import { SampleButton, HeaderNoArgs, Counter } from "./SampleComponent";
+import { SampleButton, HeaderNoArgs, Counter, RechartExample } from "./SampleComponent";
 import _ from "lodash";
 export default {
     
-    ColumnsEditor,
-    DependentTabs,
-    OperationViewer,
-    WidgetDCFCell,
-    DFViewer,
-    DFViewerInfinite,
-    StaticWrapDFViewerInfinite,
-    StatusBar,
-    HistogramCell,
-    CommandUtils,
-    utils,
-    InfiniteWidgetDCFCell: BuckarooInfiniteWidget,
-    InfiniteEx,
-    versionInfo,
-    widgetUtils,
-    SampleButton, HeaderNoArgs, Counter 
+    SampleButton, HeaderNoArgs, Counter, RechartExample
 };
 
 
