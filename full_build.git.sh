67c4d589a4fad22502779736b8a0cf5d82955c9c
Paddy Mullen <paddy@paddymullen.com>
2024-12-18 14:34:22 -0500

    WIP, full build now deletes node_modules and at least produces the same size widget.js as it does via hatch, haven't tested it yet

--- full_build.git.sh
+++ full_build.git.sh
@@ -3,6 +3,6 @@ rm -rf node_modules src/anyw2_react/static/widget.js
 rm -rf  packages/buckaroo-js-core/dist packages/buckaroo-js-core/node_modules
 cd packages/buckaroo-js-core
 npm install && npm run build
-
 cd ../..
+rm -rf packages/buckaroo-js-core/node_modules
 npm install && npm run build
